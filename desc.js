0cf78794b264345cee1b508c90e1b9e44d879a6e
Maxwell Easter <maxwelleaster00@gmail.com>
2025-08-15 04:58:03 +0100

    add project description

---
+++ desc.js
@@ -0,0 +1,72 @@
+
+/* 
+
+v1 
+core features:
+    -authentication (sign up/login, roles: buyer, seller)
+    -product mgmt (
+        seller dashboard to add/update/delete products (name, price, images, stock)
+        product detail page with images, description, price 
+    )
+    -cart & checkout (
+        add/remove from cart 
+        persist cart in redis 
+        stripe checkout 
+    )
+    -orders (
+        order creation after payment success 
+        buyer can see order history
+        seller can see orders for their products 
+    )
+    -search & filter (
+        search prodcuts by name/description
+        filter by category/price range 
+    )
+    -basic analytics (total sales & number of orders)
+
+
+
+add-ons:
+    -marketplace escrow 
+    -custom multi-seller storefront pages 
+    -advanced recommendation engine 
+    -ratings & reviews 
+    -warehouse stock management 
+    -internationalization & multi-currency 
+    -ai-generated product descriptions 
+    -uBoost (automatic product ads for SM)
+
+
+why uCommerce over other platforms?
+    -instant store creation (under 5 mins)
+    -embedded payment flexibility 
+    -lower seller fees 
+    -hybrid marketplace + storefront 
+    -ai seller tools 
+    -buyer rewards (uCoin; used for discounts)
+    -hyperfast checkout 
+    -built-in logistics partnerships 
+
+
+frontend:
+    -sveltekit, tailwind, gsap
+
+backend:    
+    -node & express, 
+    -postgres & prisma, 
+    -redis, 
+    -jwt & bcrypt 
+
+notification: 
+    -pusher 
+    -sendgrid 
+    -web push api 
+
+payments:
+    -paystack 
+
+media storage: 
+    -cloudinary
+
+
+*/
